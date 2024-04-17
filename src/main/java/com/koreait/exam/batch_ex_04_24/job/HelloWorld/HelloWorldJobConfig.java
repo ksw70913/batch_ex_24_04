ee144fd295a4ea8d13e67e1891d66f11f6eeb5b8
ksw70913 <ksw70913@gmail.com>
2024-04-17 13:35:32 +0900

    배치 관련 DB, incrementer 설정, 매 실행시도마다 Job이 실행되도록

--- src/main/java/com/koreait/exam/batch_ex_04_24/job/HelloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/koreait/exam/batch_ex_04_24/job/HelloWorld/HelloWorldJobConfig.java
@@ -1,11 +1,11 @@
-package com.koreait.exam.batch_ex_04_24.job.HelloWorld;
-
+package com.koreait.exam.batch_ex_24_04.job.HelloWorld;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -13,28 +13,34 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class helloWorldConfig {
+public class HelloWorldJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
     public Job helloWorldJob() {
-       return jobBuilderFactory.get("helloWorldJob").start(helloWorldStep1()).build();
+        return jobBuilderFactory.get("helloWorldJob")
+                .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID 를 실행할 때 파라미터로 부여
+                .start(helloWorldStep1())
+                .build();
     }
 
     @Bean
     public Step helloWorldStep1() {
-        return stepBuilderFactory.get("helloWorldStep1").tasklet(helloWorldTasklet()).build();
+        return stepBuilderFactory
+                .get("helloWorldStep1")
+                .tasklet(helloWorldTasklet())
+                .build();
     }
 
     @Bean
     public Tasklet helloWorldTasklet() {
-        return (contribution, chunkContext) -> {
+        return (stepContribution, chunkContext) -> {
+
             System.out.println("헬로월드!!!");
+
             return RepeatStatus.FINISHED;
         };
     }
-    //Job : 여러가지의 Step들로 구성
-
-
+//     Job : 여러가지의 Step들로 구성
 }
