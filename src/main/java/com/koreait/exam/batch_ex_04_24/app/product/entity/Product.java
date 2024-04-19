a2dae37baab6ca0e9b40f9aa2cbf6ee6dce9dead
ksw70913 <ksw70913@gmail.com>
2024-04-19 15:27:58 +0900

    도매가 추가, 노출용 색상, 사이즈 추가

--- src/main/java/com/koreait/exam/batch_ex_04_24/app/product/entity/Product.java
+++ src/main/java/com/koreait/exam/batch_ex_04_24/app/product/entity/Product.java
@@ -19,6 +19,7 @@ import static javax.persistence.CascadeType.ALL;
 @ToString(callSuper = true)
 public class Product extends BaseEntity {
     private int price;
+    private int wholesalePrice;
     private String name;
     private String makerShopName;
     private boolean isSoldOut; // 관련 옵션들이 전부 판매불가 상태
@@ -30,6 +31,7 @@ public class Product extends BaseEntity {
     public void addOption(ProductOption option) {
         option.setProduct(this);
         option.setPrice(getPrice());
+        option.setWholesalePrice(getWholesalePrice());
 
         productOptions.add(option);
     }
